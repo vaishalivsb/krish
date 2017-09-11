73956863f3c272cbc48804b04790ba02c31414e5
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2017-09-11 16:14:05 +0530

    Update and rename vow.c to alpha.c

---
+++ alpha.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+char s;
+scanf("%c",&s);
+  if((a<=s&&(z>=s)||(A<=s&&Z>=s))
+printf("alphabet");
+else
+printf("constant");
+}
--- vow.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-int s;
-if((s=a||s=e||s=i||s=o||s=u)&&(s=A||s=E||s=I||s=O||s=U))
-printf("vowels");
-else
-printf("consonant");
-}
