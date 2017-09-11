2497d55f0c0a51ebdb591423bc6b298f95a9374a
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2017-09-11 16:25:35 +0530

    Update and rename alpha.c to num.c

---
+++ num.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+int a,s,c;
+scanf("%d%d%d",&a,&s,&c);
+  if((a>s)&&(a>c))
+    printf("a is big");
+  elseif(s>c)
+    printf("s is big");
+else
+printf("c is big"); 
+}
