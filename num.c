843740280d206cea4a3514cadab511d88d7a105d
vaishalivsb <31504057+vaishalivsb@users.noreply.github.com>
2017-09-11 16:28:53 +0530

    Update num.c

--- num.c
+++ num.c
@@ -4,9 +4,15 @@ main()
 int a,s,c;
 scanf("%d%d%d",&a,&s,&c);
   if((a>s)&&(a>c))
-    printf("a is big");
-  elseif(s>c)
-    printf("s is big");
-else
-printf("c is big"); 
+  { 
+  printf("a is big");
+  }
+    elseif(s>c)
+    {
+      printf("s is big");
+    }
+      else
+      {
+        printf("c is big"); 
+}
 }
